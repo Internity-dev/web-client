090811becaa6642ee864c388013d8b1cbeaa62ec
kokichi17 <rafie0917@gmail.com>
2024-01-11 17:49:16 +0700

    fix(state): fix state refetch on window focus

--- src/Main.jsx
+++ src/Main.jsx
@@ -1,13 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { QueryClient, QueryClientProvider } from 'react-query';
-import './index.css';
-import { ContextProvider } from './context/ContextProvider';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { QueryClient, QueryClientProvider } from "react-query";
+import "./index.css";
+import { ContextProvider } from "./context/ContextProvider";
+import App from "./App";
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    },
+  },
+});
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
