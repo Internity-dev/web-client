91394c4bfd7000b635b9b9e9d9ec48ab097061a8
kokichi17 <rafie0917@gmail.com>
2024-08-13 22:09:46 +0700

    add news url

--- src/components/news/NewsCard.jsx
+++ src/components/news/NewsCard.jsx
@@ -13,11 +13,13 @@ const NewsCard = ({ news }) => {
     <Link to={`/news/${news.id}`} className='relative mt-3 mx-2 cursor-grab'>
       <div className='news-card w-[320px] md:w-[350px] bg-slate-100 dark:bg-[rgb(66,70,77)] cursor-pointer transition duration-300'>
         <div className='news-image'>
-          <img src={news.image} alt='news cover' />
+          <img src={news?.image} alt='news cover' />
         </div>
         <div className='lg:px-4 lg:pt-2 lg:pb-11 px-7 pt-7 pb-8'>
           <div className='flex flex-wrap mb-1'>
-            <h5 className='text-main text-xs font-medium mr-auto'>{formattedDate}</h5>
+            <h5 className='text-main text-xs font-medium mr-auto'>
+              {formattedDate}
+            </h5>
           </div>
           <h3 className='lg:text-2xl text-2xl leading-none text-dark transition duration-300 dark:text-lightOne font-semibold two-line'>
             {news.title}
