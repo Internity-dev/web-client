c2d548bf5c23867d7211d5f4852ab616b7d41249
seiyanz16 <zaskiafitrisholehah@gmail.com>
2025-02-04 10:55:38 +0700

    feat: update export button & endpoint handling

--- src/components/report/ButtonLoading.jsx
+++ src/components/report/ButtonLoading.jsx
@@ -5,7 +5,7 @@ const ButtonLoading = () => {
   return (
     <div className="flex items-center justify-center space-x-2">
       <Icon icon="eos-icons:loading" className="text-gray-600 dark:text-gray-300 text-xl animate-spin" />
-      <span className="text-sm text-gray-600 dark:text-gray-300">
+      <span className="hidden md:block text-sm text-gray-600 dark:text-gray-300">
         Processing...
       </span>
     </div>
