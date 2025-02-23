c4d5783faa62e9cafd9a099707b63007518c504b
seiyanz16 <zaskiafitrisholehah@gmail.com>
2025-02-23 16:03:43 +0700

    fix: add nis

--- src/pages/profile/ChangeProfile.jsx
+++ src/pages/profile/ChangeProfile.jsx
@@ -39,6 +39,7 @@ const ChangeProfile = () => {
   const [error, setError] = useState(null);
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
+  const [nis, setNis] = useState("");
   const [address, setAddress] = useState("");
   const [phone, setPhone] = useState("");
   const [gender, setGender] = useState("male");
@@ -57,6 +58,7 @@ const ChangeProfile = () => {
       setDateOfBirth(user.date_of_birth || "");
       setBio(user.bio || "");
       setSkills(user.skills || "");
+      setNis(user.nis || "");
     }
   }, [user]);
 
@@ -81,6 +83,7 @@ const ChangeProfile = () => {
       date_of_birth: dateOfBirth,
       bio,
       skills,
+      nis
     };
 
     try {
@@ -114,45 +117,45 @@ const ChangeProfile = () => {
   }, [message, navigate]);
 
   return (
-    <div className='m-2 md:m-10 mt-24 shadow-xl transition duration-300 dark:bg-secondary-dark-bg bg-white rounded-3xl'>
-      <form onSubmit={onSubmit} encType='multipart/form-data'>
-        <div className='w-full flex flex-col'>
-          <div className='my-10 flex flex-col justify-center items-center'>
-            <div className='relative'>
-              <div className='avatar static'>
-                <div className='w-36 rounded-full'>
+    <div className="m-2 md:m-10 mt-24 shadow-xl transition duration-300 dark:bg-secondary-dark-bg bg-white rounded-3xl">
+      <form onSubmit={onSubmit} encType="multipart/form-data">
+        <div className="w-full flex flex-col">
+          <div className="my-10 flex flex-col justify-center items-center">
+            <div className="relative">
+              <div className="avatar static">
+                <div className="w-36 rounded-full">
                   <img
                     src={selectedImage ? selectedImage : user?.avatar_url}
-                    className='rounded-full w-36 h-36 min-w-full min-h-full max-w-36 max-h-36 object-fill'
+                    className="rounded-full w-36 h-36 min-w-full min-h-full max-w-36 max-h-36 object-fill"
                   />
                 </div>
               </div>
-              <div className='absolute bottom-0 right-0 flex justify-center items-center bg-main overflow-hidden rounded-full w-11 h-11'>
+              <div className="absolute bottom-0 right-0 flex justify-center items-center bg-main overflow-hidden rounded-full w-11 h-11">
                 <input
-                  type='file'
-                  name='avatar'
-                  accept='image/*'
+                  type="file"
+                  name="avatar"
+                  accept="image/*"
                   onChange={handleImageChange}
                   ref={imageInputRef}
-                  className='absolute opacity-0 transform scale-150 cursor-pointer'
+                  className="absolute opacity-0 transform scale-150 cursor-pointer"
                 />
                 <Icon
-                  icon='mdi:camera-enhance'
-                  className='text-lightOne'
+                  icon="mdi:camera-enhance"
+                  className="text-lightOne"
                   width={30}
                 />
               </div>
             </div>
           </div>
         </div>
-        <div className='flex flex-col'>
-          <div className='mx-10 my-5'>
+        <div className="flex flex-col">
+          <div className="mx-10 my-5">
             {message && <Alert text={message} />}
             {error && <Alert text={error} error />}
             <InputText
-              label='Nama'
-              name='name'
-              type='text'
+              label="Nama"
+              name="name"
+              type="text"
               innerRef={nameRef}
               placeholder={user?.name ? user.name : "Masukkan Nama"}
               value={name}
@@ -160,9 +163,9 @@ const ChangeProfile = () => {
             />
 
             <InputText
-              label='Email'
-              name='email'
-              type='email'
+              label="Email"
+              name="email"
+              type="email"
               innerRef={emailRef}
               placeholder={user?.email ? user.email : "Masukkan Email"}
               value={email}
@@ -170,9 +173,9 @@ const ChangeProfile = () => {
             />
 
             <InputText
-              label='Alamat'
-              name='address'
-              type='text'
+              label="Alamat"
+              name="address"
+              type="text"
               innerRef={addressRef}
               placeholder={user?.address ? user.address : "Masukkan Alamat"}
               value={address}
@@ -180,36 +183,46 @@ const ChangeProfile = () => {
             />
 
             <InputText
-              label='Nomor Telepon'
-              name='phone'
-              type='text'
+              label="Nomor Telepon"
+              name="phone"
+              type="text"
               innerRef={phoneRef}
               placeholder={user?.phone ? user.phone : "Masukkan Nomor Telepon"}
               value={phone}
               onChange={(e) => setPhone(e.target.value)}
             />
 
-            <div className='form-control w-full my-2'>
-              <label className='label'>
-                <span className='label-text text-dark transition duration-300 dark:text-lightOne text-base'>
+            <InputText
+              label="NIS"
+              name="nis"
+              type="text"
+              innerRef={nameRef}
+              placeholder={user?.nis ? user.nis : "Masukkan NIS"}
+              value={nis}
+              onChange={(e) => setNis(e.target.value)}
+            />
+
+            <div className="form-control w-full my-2">
+              <label className="label">
+                <span className="label-text text-dark transition duration-300 dark:text-lightOne text-base">
                   Jenis Kelamin
                 </span>
               </label>
               <select
-                name='gender'
+                name="gender"
                 ref={genderRef}
-                className='select select-bordered bg-main-bg transition duration-300 dark:bg-main-dark-bg text-base'
+                className="select select-bordered bg-main-bg transition duration-300 dark:bg-main-dark-bg text-base"
                 value={gender}
                 onChange={(e) => setGender(e.target.value)}
               >
                 <option disabled>Pilih jenis kelamin kamu</option>
-                <option value='male'>Laki-Laki</option>
-                <option value='female'>Perempuan</option>
+                <option value="male">Laki-Laki</option>
+                <option value="female">Perempuan</option>
               </select>
             </div>
             <InputDate
-              label='Tanggal Lahir'
-              name='date_of_birth'
+              label="Tanggal Lahir"
+              name="date_of_birth"
               innerRef={dateOfBirthRef}
               placeholder={
                 user?.date_of_birth ? user.date_of_birth : "17/09/2006"
@@ -218,15 +231,15 @@ const ChangeProfile = () => {
               onChange={(e) => setDateOfBirth(e.target.value)}
             />
 
-            <div className='form-control my-2'>
-              <label className='label'>
-                <span className='label-text text-dark transition duration-300 dark:text-lightOne text-base'>
+            <div className="form-control my-2">
+              <label className="label">
+                <span className="label-text text-dark transition duration-300 dark:text-lightOne text-base">
                   Tentang Saya
                 </span>
               </label>
               <textarea
-                name='bio'
-                className='textarea textarea-bordered h-24 bg-main-bg transition duration-300 dark:bg-main-dark-bg text-base'
+                name="bio"
+                className="textarea textarea-bordered h-24 bg-main-bg transition duration-300 dark:bg-main-dark-bg text-base"
                 ref={bioRef}
                 placeholder={user?.bio ? user.bio : "Masukkan Bio"}
                 value={bio}
@@ -235,15 +248,15 @@ const ChangeProfile = () => {
             </div>
 
             <InputText
-              label='Skills'
-              name='skills'
-              type='text'
+              label="Skills"
+              name="skills"
+              type="text"
               innerRef={skillsRef}
               placeholder={user?.skills ? user.skills : "Masukkan Skills"}
               value={skills}
               onChange={(e) => setSkills(e.target.value)}
             />
-            <LoginBtn text='Save' />
+            <LoginBtn text="Save" />
           </div>
         </div>
       </form>
